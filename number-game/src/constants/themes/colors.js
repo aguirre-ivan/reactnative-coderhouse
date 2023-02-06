8884a81cc349767615cf59e5944c49cbe876965f
Ivan Aguirre <74165953+aguirre-ivan@users.noreply.github.com>
2023-02-06 07:03:19 -0300

    Number game finished (#1)

---
+++ number-game/src/constants/themes/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+  primary: "#2F4550",
+  secondary: "#586F7C",
+  background: "#B8DBD9",
+  text: "#212121",
+  white: "#F4F4F9",
+  black: "#000000",
+};
---
+++ number-game/src/constants/themes/paddings.js
@@ -0,0 +1,6 @@
+export const paddings = {
+  small: 6,
+  medium: 14,
+  large: 22,
+  xlarge: 30,
+};
